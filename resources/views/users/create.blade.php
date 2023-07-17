746f95df8e490e8f56a37d6f7e5782bb7e83fbf2
Z1ZA <sayaabdaziz1@gmail.com>
2023-07-17 14:16:22 +0700

    convert page login & register to blade

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,31 +1,54 @@
-<html>
-<head>
-    <title>Register</title>
-</head>
-<body>
-<h1>Register</h1>
-<form action="/users" method="post">
-    @csrf
-    @method('POST')
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" value="{{old('name')}}">
-    @error('name')
-    <p style="color: red; font-size: small;">{{ $message }}</p>
-    @enderror
-
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" value="{{old('email')}}">
-    @error('email')
-    <p style="color: red; font-size: small;">{{ $message }}</p>
-    @enderror
-
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" value="{{old('password')}}">
-    @error('password')
-    <p style="color: red; font-size: small;">{{ $message }}</p>
-    @enderror
-
-    <button type="submit">Register</button>
-</form>
-</body>
-</html>
+<x-layout>
+    <header class="pt-8">
+        <div class="md:container">
+            <nav class="flex items-center justify-between">
+                <a href="#">
+                    <img class="w-40" src="{{asset('images/logo.png')}}" alt="logo" />
+                </a>
+            </nav>
+        </div>
+    </header>
+    <div class="pt-14">
+        <div class="flex flex-col items-center justify-center gap-4">
+            <img src="{{asset('images/logo-daun.png')}}" alt="" class="w-16" />
+            <h1 class="font-playfair text-[32px]">Register to Naturally</h1>
+            <p class="font-work text-[16px]">Create account to join us!</p>
+        </div>
+    </div>
+    <section class="p-14">
+        <form action="/users" method="post">
+            @csrf
+            <div class="flex flex-col items-center font-work text-xl">
+                <div class="flex flex-col w-[40%] gap-4">
+                    <p>Name</p>
+                    <input type="text" name="name" id="name" value="{{old('name')}}" class="rounded-xl h-16 bg-[#f1f1f1] focus:outline-0 focus:ring-1 focus:ring-hijau-200 px-4" />
+                    @error('name')
+                    <p style="color: red; font-size: small;">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex flex-col w-[40%] gap-4 pt-6">
+                    <p>Email</p>
+                    <input type="email" name="email" id="email" value="{{old('email')}}" class="rounded-xl h-16 bg-[#f1f1f1] focus:outline-0 focus:ring-1 focus:ring-hijau-200 px-4" />
+                    @error('email')
+                    <p style="color: red; font-size: small;">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex flex-col w-[40%] gap-4 pt-6">
+                    <p>Password</p>
+                    <input type="password" name="password" id="password" value="{{old('email')}}" class="rounded-xl h-16 bg-[#f1f1f1] focus:outline-0 focus:ring-1 focus:ring-hijau-200 px-4" />
+                    @error('password')
+                    <p style="color: red; font-size: small;">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button class="bg-hijau-200 p-5 w-[40%] rounded-xl mt-12">
+                    <a href="#" class="text-[#ffffff]">Register</a>
+                </button>
+                <div class="flex pt-16 text-base">
+                    <p class="text-[#717171]">Already have an account?</p>
+                    &nbsp;
+                    <a href="/loginForm" class="text-hijau-100">Log in!</a>
+                </div>
+            </div>
+        </form>
+    </section>
+</x-layout>
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,26 +1,53 @@
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-<h1>Login</h1>
-<form action="/users/authenticate" method="post">
-    @csrf
-    @method('POST')
-
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" value="{{old('email')}}">
-    @error('email')
-    <p style="color: red; font-size: small;">{{ $message }}</p>
-    @enderror
-
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" value="{{old('password')}}">
-    @error('password')
-    <p style="color: red; font-size: small;">{{ $message }}</p>
-    @enderror
-
-    <button type="submit">Login</button>
-</form>
-</body>
-</html>
+<x-layout>
+    <header class="pt-8">
+        <div class="md:container">
+            <nav class="flex items-center justify-between">
+                <a href="#">
+                    <img class="w-40" src="images/logo.png" alt="logo" />
+                </a>
+                <a href="#" class="bg-hijau-200 px-8 py-4 font-work font-medium text-white rounded-xl">
+                    Register
+                </a>
+            </nav>
+        </div>
+    </header>
+    <div class="pt-14">
+        <div class="flex flex-col items-center justify-center gap-4">
+            <img src="images/logo-daun.png" alt="" class="w-16" />
+            <h1 class="font-playfair text-[32px]">Welcome to Naturally</h1>
+            <p class="font-work text-[16px]">
+                Login to Naturally to get unforgetable experience!
+            </p>
+        </div>
+    </div>
+    <!-- login section -->
+    <section class="p-14">
+        <form action="/users/authenticate" method="post">
+            <div class="flex flex-col items-center font-work text-xl">
+                @csrf
+                <div class="flex flex-col w-[40%] gap-4">
+                    <p>Email</p>
+                    <input type="email" name="email" id="email" value="{{old('email')}}" class="rounded-xl h-16 bg-[#f1f1f1] focus:outline-0 focus:ring-1 focus:ring-hijau-200 px-4" />
+                    @error('email')
+                    <p style="color: red; font-size: small;">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex flex-col w-[40%] gap-4 pt-6">
+                    <p>Password</p>
+                    <input type="password" name="password" id="password" value="{{old('password')}}" class="rounded-xl h-16 bg-[#f1f1f1] focus:outline-0 focus:ring-1 focus:ring-hijau-200 px-4" />
+                    @error('password')
+                    <p style="color: red; font-size: small;">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button class="bg-hijau-200 p-5 w-[40%] rounded-xl mt-12">
+                    <a href="#" class="text-[#ffffff]">Login</a>
+                </button>
+                <div class="flex pt-16 text-base">
+                    <p class="text-[#717171]">Have no account?</p>
+                    &nbsp;
+                    <a href="/registerForm" class="text-hijau-100">Register!</a>
+                </div>
+            </div>
+        </form>
+    </section>
+</x-layout>
