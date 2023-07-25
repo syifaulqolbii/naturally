1de2f3adaa7f830a34f6342aa06ecc9b715931e1
Z1ZA <sayaabdaziz1@gmail.com>
2023-07-25 17:16:01 +0700

    edit create_activity.blade

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500&family=Work+Sans:wght@400;500&display=swap"
-        rel="stylesheet"
-    />
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <link
-        href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css"
-        rel="stylesheet"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500&family=Work+Sans:wght@400;500&display=swap" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
@@ -55,13 +50,15 @@
     <script src="//unpkg.com/alpinejs" defer></script>
     <title>Naturally</title>
 </head>
+
 <body>
 
-{{--View Output--}}
-<main>
-    {{$slot}}
-</main>
+    {{--View Output--}}
+    <main>
+        {{$slot}}
+    </main>
 
-<x-flash-message/>
+    <x-flash-message />
 </body>
+
 </html>
